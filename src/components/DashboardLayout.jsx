b644b6ee201d19c285c201ce779a591b18f33fb6
vincentheimann <heimvin@gmail.com>
2025-03-05 12:08:20 +0100

    Fix button props

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -143,31 +143,58 @@ const DashboardLayout = ({ children, handleThemeChange, themeMode }) => {
       </DrawerHeader>
       <Divider />
       <List>
-        <ListItem button="true" component={Link} to="/" onClick={handleDrawerClose}>
+        <ListItem
+          button="true"
+          component={Link}
+          to="/"
+          onClick={handleDrawerClose}
+        >
           <ListItemIcon>
             <Home /> {/* Home icon */}
           </ListItemIcon>
           <ListItemText primary="Home" />
         </ListItem>
-        <ListItem button="true" component={Link} to="/german" onClick={handleDrawerClose}>
+        <ListItem
+          button="true"
+          component={Link}
+          to="/german"
+          onClick={handleDrawerClose}
+        >
           <ListItemIcon>
             <MenuBook />
           </ListItemIcon>
           <ListItemText primary="German" />
         </ListItem>
-        <ListItem button="true" component={Link} to="/german/b2" sx={{ pl: 4 }} onClick={handleDrawerClose}>
+        <ListItem
+          button="true"
+          component={Link}
+          to="/german/b2"
+          sx={{ pl: 4 }}
+          onClick={handleDrawerClose}
+        >
           <ListItemIcon>
             <ExpandMore />
           </ListItemIcon>
           <ListItemText primary="B2" />
         </ListItem>
-        <ListItem button="true" component={Link} to="/english" onClick={handleDrawerClose}>
+        <ListItem
+          button="true"
+          component={Link}
+          to="/english"
+          onClick={handleDrawerClose}
+        >
           <ListItemIcon>
             <MenuBook />
           </ListItemIcon>
           <ListItemText primary="English" />
         </ListItem>
-        <ListItem button="true" component={Link} to="/english/c1" sx={{ pl: 4 }} onClick={handleDrawerClose}>
+        <ListItem
+          button="true"
+          component={Link}
+          to="/english/c1"
+          sx={{ pl: 4 }}
+          onClick={handleDrawerClose}
+        >
           <ListItemIcon>
             <ExpandMore />
           </ListItemIcon>
