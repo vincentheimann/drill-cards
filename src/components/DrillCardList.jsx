0ab02ebe7b6212a2cce267aed2143c31b0e97f77
vincentheimann <heimvin@gmail.com>
2025-03-05 09:30:13 +0100

    Fix Switch All

--- src/components/DrillCardList.jsx
+++ src/components/DrillCardList.jsx
@@ -11,15 +11,36 @@ import DrillCard from "./DrillCard";
 import { useCards } from "../context/CardsContext";
 
 export default function DrillCardList() {
+  const [switchAll, setSwitchAll] = useState(false);
+  const handleChange = () => {
+    setSwitchAll((prev) => !prev);
+  };
   const { cards, moveCardToEnd, moveCardToTop } = useCards();
 
   return (
     <Box>
       <Grid container spacing={4}>
+        <Grid size={12}>
+          <FormControl component="fieldset" variant="standard">
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={switchAll}
+                    onChange={handleChange}
+                    name="switch-all"
+                  />
+                }
+                label="Switch All Cards"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
         {cards.map((card, index) => (
-          <Grid xs="auto" key={index}>
+          <Grid size="auto" key={index}>
             <DrillCard
               word={card}
+              switchAll={switchAll}
               moveCardToEnd={() => moveCardToEnd(index)}
               moveCardToTop={() => moveCardToTop(index)}
             />
